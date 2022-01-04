4328fa707d402b1c71ab37ab9cbaa7386a4e533d
MaksymChuraiev <mchuraev1988@gmail.com>
2022-01-04 23:31:04 +0200

    add countries list

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,13 @@
 function fetchCountries(name) {
-  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages `;
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `;
 
-  return fetch(url).then(response => response.json());
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
 }
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -13,37 +13,52 @@ const refs = {
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
-// Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-// Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 
 refs.input.addEventListener('input', debounce(onChangeCountry, DEBOUNCE_DELAY));
 
 function onChangeCountry(e) {
   e.preventDefault();
 
-  const form = e.target.value;
+  let search = refs.input.value.trim();
+  if (search === '') {
+    clear();
+    return;
+  }
 
-  // fetchCountries(form).then(responce => {
-  //   if (responce.length > 10) {
-  //     Notify.failure('Too many matches found. Please enter a more specific name.');
-  //   }
-  //   // renderCountryCart;
-
-  //   // if (responce.length > 1) {
-  //   //   renderCountryList;
-  //   // }
-  //   // if (responce.length === 1) {
-  //   //   renderCountryCart;
-  //   // }
-  // });
+  fetchCountries(search)
+    .then(responce => {
+      if (responce.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      }
+      if (responce.length > 1 && responce.length <= 10) {
+        renderCountryList(responce);
+      }
+      if (responce.length === 1) {
+        renderCountryCart(responce);
+      }
+    })
+    .catch(error => {
+      clear();
+      Notify.failure('Oops, there is no country with that name');
+      return error;
+    });
 }
 
 function renderCountryCart(country) {
   const markup = itemsTemplate(country);
+
+  refs.countryList.innerHTML = '';
   refs.countryInfo.innerHTML = markup;
 }
 
 function renderCountryList(countries) {
-  const markupList = countriesTemplate;
+  const markupList = countriesTemplate(countries);
+
+  refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = markupList;
+}
+
+function clear() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
